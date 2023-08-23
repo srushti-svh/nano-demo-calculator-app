f48e19b54dbfa18f7549036efab78bff8dd0ba52
PES1UG20C37_Srushti_S_Hampannavar <srushtihampannavar@gmail.com>
2023-08-23 23:08:04 +0530

    update

--- routes.cpp
+++ routes.cpp
@@ -165,7 +165,7 @@ int main()
     app.port(8080).multithreaded().run();
     return 0;
 }
-*/
+
 
 #include "crow_all.h"
 
@@ -208,4 +208,50 @@ int main()
 
     app.port(8080).multithreaded().run();
     return 0;
+}
+*/
+
+#include "crow_all.h"
+
+crow::response greet()
+{
+    return crow::response{"Hello world!"};
+}
+
+crow::response add(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first + second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
+}
+
+crow::response subtract(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+
+    if (!input)
+    {
+        return crow::response(400, "Invalid JSON");
+    }
+
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first - second;
+
+    crow::json::wvalue response;
+    response["result"] = result;
+
+    return crow::response{response};
 }
