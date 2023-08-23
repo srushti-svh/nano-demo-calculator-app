e39cfb2e3750538919fff209f4f33b468f69cb46
PES1UG20C37_Srushti_S_Hampannavar <srushtihampannavar@gmail.com>
2023-08-23 21:34:47 +0530

    update

--- routes.cpp
+++ routes.cpp
@@ -64,6 +64,7 @@ int main()
 }
 */
 
+/*
 #include "crow_all.h"
 
 int main()
@@ -105,6 +106,59 @@ int main()
         crow::json::wvalue responseJson;
         responseJson["result"] = result;
         return crow::response(responseJson);
+    });
+
+    app.port(8080).multithreaded().run();
+    return 0;
+}
+
+*/
+
+#include "crow_all.h"
+
+crow::response greet()
+{
+    return crow::response{""};
+}
+
+crow::response add(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+    int firstNum = input["first"].i();
+    int secondNum = input["second"].i();
+    int res = firstNum + secondNum;
+    
+    crow::json::wvalue responseJson;
+    responseJson["result"] = res;
+    return crow::response{crow::json::dump(responseJson)};
+}
+
+crow::response subtract(const crow::request &req)
+{
+    auto input = crow::json::load(req.body);
+    int firstNum = input["first"].i();
+    int secondNum = input["second"].i();
+    int res = firstNum - secondNum;
+
+    crow::json::wvalue responseJson;
+    responseJson["result"] = res;
+    return crow::response{crow::json::dump(responseJson)};
+}
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/calculator/greeting").methods("GET"_method)([]() {
+        return greet();
+    });
+
+    CROW_ROUTE(app, "/calculator/add").methods("POST"_method)([](const crow::request &req) {
+        return add(req);
+    });
+
+    CROW_ROUTE(app, "/calculator/subtract").methods("POST"_method)([](const crow::request &req) {
+        return subtract(req);
     });
 
     app.port(8080).multithreaded().run();
